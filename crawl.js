3f90358150019bd9d5a97fa91bdf0b91c0023f13
Nansamba Ssensalo <nssensalo@gmail.com>
2025-05-07 16:09:06 -0700

    adding await to crawl function on main.js,logging of pages from main,stopped getRRL logging

--- crawl.js
+++ crawl.js
@@ -12,29 +12,65 @@ crawlPage()
 This is first crawl a single page. Next will be crawling a whole website.
 Dont do "If error code less than 300 try it", instead stop program if error code is high.
 Check if valid html ( content type is html/text)
+Make sure baseUrl is same as domain for currentUrl. Pages helps us keep track of 
 `
 
-async function crawlPage( currentUrl) {
+async function crawlPage( baseUrl,currentUrl, pages) {
+    const baseUrlObj = new URL(baseUrl)
+    const currentUrlObj = new URL(currentUrl)
+   
+    if ( baseUrlObj.hostname !== currentUrlObj.hostname) {
+        return pages // an object the maps the url to a number "my.website": 2
+    }
+    console.log("1. Completed hostname check!")
+    const normalizedCurrentUrl  = normalizeUrl(currentUrl) // get regular non object url 
+    
+    if(pages[normalizedCurrentUrl] > 0 ){ //check if the pages object of that url is mapped to a number > 0 (its been visited)
+        pages[normalizedCurrentUrl]++ //if so, then increment "counter"
+        return pages //return the object for continued use
+    }
+    //console.log("PAGES:",JSON.stringify(pages,null,2))
+    for (const [key, value] of Object.entries(pages)) {
+        console.log(`2. Page: ${key}, Visits: ${value}`);
+      }
+    
+        
+       
+    pages[normalizedCurrentUrl] = 1
+
+    console.log(`3. Actively Crawling: ${currentUrl}`) 
+
     try {
-        console.log(`actively crawling: ${currentUrl}`)
+        
         const resp = await fetch(currentUrl)
 
         const contentType = resp.headers.get("content-type")
-        console.log(`ContentType:${contentType}`)
-
+      
+        console.log("4. Checking status response code...")
         if(resp.status > 399){
            console.log(`Error ${resp.status} fetching sit:${currentUrl}`)
-           return
+           return pages
         }
+        
+        console.log("5. Checking content type...")
         if( !contentType.includes("text/html")){ //NOTE: remember in js !== , and !object
             console.log(`Error, non html response w/ content type: ${contentType} for site:${currentUrl}`)
-            return
+            return pages
         }
-    
+       
+        console.log("6. Getting text...")
+        const htmlBody = await resp.text()// NOTE: without await it was pending 
         
-        console.log( await resp.text()) // NOTE: without await it was pending 
+        const nextUrls  = getUrlsFromHTML(htmlBody, baseUrl)
+        console.log("7. Getting recursive urls...")
+        for ( nextUrl of nextUrls ) {    
+            console.log(`Recursive url: ${nextUrl}`)                   // Now, crawl the pages of the pages until we cover all territory
+            pages = await crawlPage(baseUrl, nextUrl, pages) // updating pages object????????????????
+            
+        }
         
-        
+        return pages
+
        
     } catch(err){
         console.log(`Error during fetch for site:${currentUrl}. ${err.message}`)
@@ -70,7 +106,7 @@ function getUrlsFromHTML (htmlBody, baseUrl) {
             try{
                 //absolute
                 const objUrl = new URL(linkElement.href)
-                console.log(objUrl)
+                //console.log(objUrl)
                 urls.push(objUrl.href)
             } catch(err) {
                 console.log(`Error with absolute url:${err.message}`) 
@@ -81,7 +117,7 @@ function getUrlsFromHTML (htmlBody, baseUrl) {
 }
 
 
-function normalizeUrl(urlString) {
+function normalizeUrl(urlString) { //removing trailing slash at end of path
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
     if( hostPath.length > 0 && hostPath.slice(-1)==='/'){
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { crawlPage } = require('./crawl.js');
 
 //access command line argument 
 //check that it has three parts
-function main() {
+async function main() {
     if (process.argv.length < 3) {
 
         console.log('No website provided')
@@ -18,8 +18,12 @@ function main() {
         }
     }
     const baseUrl = process.argv[2]
-   console.log(`Starting web crawl for: ${baseUrl}`) 
-   crawlPage(baseUrl)
    
+    console.log(`Starting web crawl for: ${baseUrl}`) 
+    const pages = await crawlPage(baseUrl, baseUrl, {}) // the empty object is the pages object, the 2nd baseUrl is the currentUrl
+    
+    for ( const page of Object.entries(pages)) {     //NOTE: cant do for page of pages because pages not an array, its an object. Entries allows iteration within
+    console.log(page) // AND dont forget const of const page of pages
+   }
 }
 main()
